5aa41cbe3c3f06d35e11e90b192dd97c6da525f3
obibring <netbkb@gmail.com>
2014-10-26 14:02:51 -0400

    Orr committing calendar before merge

--- Flatiron-App/AddEventViewController.m
+++ Flatiron-App/AddEventViewController.m
@@ -7,15 +7,31 @@
 //
 
 #import "AddEventViewController.h"
+#define leftPadding 5
 
-@interface AddEventViewController () 
+@interface AddEventViewController ()
 @property (weak, nonatomic) IBOutlet UIBarButtonItem *saveButton;
 @property (weak, nonatomic) IBOutlet UITextField *titleInput;
 @property (weak, nonatomic) IBOutlet UITextField *isAllDayEventInput;
 @property (weak, nonatomic) IBOutlet UITextField *startDateInput;
 @property (weak, nonatomic) IBOutlet UITextField *endDateInput;
+@property (weak, nonatomic) IBOutlet UISwitch *switchInput;
 @property (strong, nonatomic) UIDatePicker *startDatePicker;
 @property (strong, nonatomic) UIDatePicker *endDatePicker;
+-(void)addPaddingToTextInputs;
++(UIView *)makePaddingView;
+-(BOOL)formIsValid;
+@end
+
+@interface PaddedLabel : UILabel
+@end
+
+@implementation PaddedLabel
+
+-(void)drawTextInRect:(CGRect)rect {
+    return [super drawTextInRect:UIEdgeInsetsInsetRect(rect, UIEdgeInsetsMake(0, leftPadding, 0, 0))];
+}
+
 @end
 
 @implementation AddEventViewController
@@ -25,19 +41,26 @@
     
     // By default, disable the save button until the event has enough information.
     self.saveButton.enabled = NO;
-   
     
     // Create date pickers as inputViews for the start end end textViews
     self.startDatePicker = [[UIDatePicker alloc] init];
     self.endDatePicker = [[UIDatePicker alloc] init];
     
-    self.startDateInput.inputView = self.startDatePicker;
-    self.endDateInput.inputView = self.endDatePicker;
+    // Make sure the end date picker is never before the start date
+    self.endDatePicker.minimumDate = self.startDatePicker.date;
     
+//    self.startDatePicker.translatesAutoresizingMaskIntoConstraints = NO;
+//    self.endDatePicker.translatesAutoresizingMaskIntoConstraints = NO;
+    
+    self.startDateInput.inputView = self.startDatePicker;
+//    self.startDateInput.inputAccessoryView = self.startDatePicker;
+    self.endDateInput.inputView = self.endDatePicker;
+//    self.endDateInput.inputAccessoryView = self.endDatePicker;
     
     // Set delegates
     self.startDateInput.delegate = self;
     self.endDateInput.delegate = self;
+    self.titleInput.delegate = self;
     
     // Add padding to text inputs
     [self addPaddingToTextInputs];
@@ -63,14 +86,47 @@
 
 #pragma mark - UITextViewDelegate
 
--(void)textViewDidEndEditing:(UITextView *)textView {
-    NSLog(@"DID END EDITING");
+-(void)textFieldDidEndEditing:(UITextField *)textField {
+    if (textField == self.startDateInput) {
+        self.startDateInput.text = [self formatDate:self.startDatePicker.date];
+    } else if (textField == self.endDateInput) {
+        self.endDateInput.text = [self formatDate:self.endDatePicker.date];
+    }
+    [self checkIfNeedsEnablingAndEnableSaveButton];
 }
 
 #pragma mark - utilities
 
+-(NSString *)formatDate:(NSDate *)date {
+    NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+    [formatter setDateStyle:NSDateFormatterLongStyle];
+    [formatter setTimeStyle:NSDateFormatterMediumStyle];
+    return [formatter stringFromDate:date];
+}
+
+// Checks whether the save button should be enabled, and if so, enables it.
+-(void)checkIfNeedsEnablingAndEnableSaveButton {
+    self.saveButton.enabled = [self formIsValid];
+}
+
+-(BOOL)formIsValid {
+    NSDate *start = self.startDatePicker.date;
+    NSDate *end = self.endDatePicker.date;
+    NSString *title = self.titleInput.text;
+    
+    if (!start || !end) { // Must Exist
+        return NO;
+    } else if ([start compare:end] != NSOrderedAscending) { // Must be in order
+        return NO;
+    } else if (!title || [title length] < 1) {
+        return NO;
+    }
+    
+    return YES;
+}
+
 +(UIView *)makePaddingView {
-   return [[UIView alloc] initWithFrame:CGRectMake(0, 0, 5, 20)];
+   return [[UIView alloc] initWithFrame:CGRectMake(0, 0, leftPadding, 20)];
 }
 
 -(void)addPaddingToTextInputs {
